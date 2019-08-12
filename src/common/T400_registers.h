01c647be632060188299ea7c5fa46a7f5d50dd8e
ksoltan <kattsoltan@gmail.com>
2019-08-12 09:55:33 -0400

    Added all T400 registers of interest.

--- src/common/T400_registers.h
+++ src/common/T400_registers.h
@@ -17,27 +17,56 @@ enum DiscreteInputFlag {
 };
 
 const std::map<int, DiscreteInputFlag> DISCRETE_INPUTS_MAP = {
-  { 0, FLAG_MONITOR },
-  { 1, FLAG_MONITOR },
-  { 8, FLAG_SAMPLE },
-  { 20, FLAG_CALIBRATION }
+  {0, FLAG_MONITOR},
+  {1, FLAG_MONITOR},
+  {2, FLAG_MONITOR},
+  {3, FLAG_MONITOR},
+  {4, FLAG_MONITOR},
+  {5, FLAG_MONITOR},
+  {6, FLAG_MONITOR},
+  {7, FLAG_MONITOR},
+  {8, FLAG_SAMPLE},
+  {9, FLAG_SAMPLE},
+  {10, FLAG_SAMPLE},
+  {11, FLAG_MONITOR},
+  {12, FLAG_MONITOR},
+  {13, FLAG_MONITOR},
+  {14, FLAG_MONITOR},
+  {15, FLAG_MONITOR},
+  {16, FLAG_MONITOR},
+  {19, FLAG_SAMPLE},
+  {20, FLAG_CALIBRATION},
+  {21, FLAG_CALIBRATION},
+  {22, FLAG_CALIBRATION},
+  {23, FLAG_CALIBRATION},
+  {25,	FLAG_SAMPLE}
 };
 
 const std::vector<int> INPUT_REGISTERS = {
-  0, /* PHSTB */
   6, /* SLOPE1 */
   10, /* OFSET1 */
-  20, /* SMPFLW */
-  124, /* CONC1 */
+  18, /* CONC1 */
+  28, /* O3TEMP */
+  30, /* O3STMP */
+  34, /* PHTEMP */
+  38, /* SMPTMP */
+  40, /* SMPFLW */
+  42, /* SMPPRS */
+  44 /* BOXTMP */
 };
 
 //
 const String SD_FILE_HEADER_STR =
-  "PHSTB,"
   "SLOPE1,"
   "OFSET1,"
-  "SMPLFLW,"
-  "CONC1,"
+  "CONC1 ,"
+  "O3TEMP,"
+  "O3STMP,"
+  "PHTEMP,"
+  "SMPTMP,"
+  "SMPFLW,"
+  "SMPPRS,"
+  "BOXTMP,"
   "FLAGS"; // Make sure to end with Flags (discrete inputs)
 
 #endif // T400_REGISTERS_H
